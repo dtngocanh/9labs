b3ca886015c7060eab047a2bdf1c9a2b2a0edfdc
anhngocthiduong <anh.dtng@gmail.com>
2025-04-12 07:39:43 +0700

    update labs

--- Lab03_DiceApp/android/app/src/main/kotlin/com/example/lab03_diceapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.lab03_diceapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lab04_magic8ballapp/android/app/src/main/kotlin/com/example/lab04_magic8ballapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.lab04_magic8ballapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
